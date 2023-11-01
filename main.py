cce10efffe11dd83f756e9540e90c22c9df7f7f0
Kamil Poszepczyński <93219293+poszepa@users.noreply.github.com>
2023-11-01 22:25:06 +0100

    change main to new lesson

--- main.py
+++ main.py
@@ -1,10 +1,22 @@
 import Enum
 import SumNumbersInRange
 import DefRunTime
+import checkNumberisInContainer
 
 if __name__ == '__main__':
  #   Enum.program_to_calculate_area()
-    print(DefRunTime.calculate_run_time(SumNumbersInRange.sumNumberInRange, 100000000,4))
-    print(DefRunTime.calculate_run_time(SumNumbersInRange.sumNumberInGenerator, 100000000,4))
-    print(DefRunTime.calculate_run_time(SumNumbersInRange.sumNumberInLoop, 100000000,4))
-    print(DefRunTime.calculate_run_time(SumNumbersInRange.sumNumberByArythmetic, 100000000,4))
+    print(DefRunTime.calculate_run_time(
+        checkNumberisInContainer.is_number_in_collection,
+        10000,
+        checkNumberisInContainer.listContainer,
+        20,
+        ))
+
+    print(DefRunTime.calculate_run_time(
+        checkNumberisInContainer.is_number_in_collection,
+        10000,
+        checkNumberisInContainer.setContainer,
+        20,
+        ))
+
+
