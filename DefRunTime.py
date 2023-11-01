debf10ab4e4dfb49f3b596804b5e2796bddeea8b
Kamil Poszepczyński <93219293+poszepa@users.noreply.github.com>
2023-11-01 19:55:05 +0100

    Change dif to use default args

--- DefRunTime.py
+++ DefRunTime.py
@@ -1,7 +1,10 @@
 import time
 
-def calculate_run_time(dif,arg):
-    start = time.perf_counter()
-    dif(arg)
-    end = time.perf_counter()
-    return (end - start)
+def calculate_run_time(dif,arg, how_many_times = 1):
+    sumTime = 0
+    for i in range(0, how_many_times):
+        start = time.perf_counter()
+        dif(arg)
+        end = time.perf_counter()
+        sumTime += (end - start)
+    return sumTime
