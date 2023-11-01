d0270e855d305ab01679781c76dbadb4f79064dd
Kamil Poszepczyński <93219293+poszepa@users.noreply.github.com>
2023-11-01 19:55:18 +0100

    change method to use default args

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import DefRunTime
 
 if __name__ == '__main__':
  #   Enum.program_to_calculate_area()
-    print(DefRunTime.calculate_run_time(SumNumbersInRange.sumNumberInRange, 100000000))
-    print(DefRunTime.calculate_run_time(SumNumbersInRange.sumNumberInGenerator, 100000000))
-    print(DefRunTime.calculate_run_time(SumNumbersInRange.sumNumberInLoop, 100000000))
-    print(DefRunTime.calculate_run_time(SumNumbersInRange.sumNumberByArythmetic, 100000000))
+    print(DefRunTime.calculate_run_time(SumNumbersInRange.sumNumberInRange, 100000000,4))
+    print(DefRunTime.calculate_run_time(SumNumbersInRange.sumNumberInGenerator, 100000000,4))
+    print(DefRunTime.calculate_run_time(SumNumbersInRange.sumNumberInLoop, 100000000,4))
+    print(DefRunTime.calculate_run_time(SumNumbersInRange.sumNumberByArythmetic, 100000000,4))
