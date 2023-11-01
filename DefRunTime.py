9fbaacf9d2599102726ea66a8326aa482374522c
Kamil Poszepczyński <93219293+poszepa@users.noreply.github.com>
2023-11-01 22:24:39 +0100

    change argument name from dif on func

--- DefRunTime.py
+++ DefRunTime.py
@@ -1,20 +1,20 @@
 import time
 
-def calculate_run_time(dif,arg, how_many_times = 1):
+def calculate_run_time(func, how_many_times = 1, *arg):
     sumTime = 0
     for i in range(0, how_many_times):
         start = time.perf_counter()
-        dif(arg)
+        func(*arg)
         end = time.perf_counter()
         sumTime += (end - start)
     return sumTime
 
 
-def calculate_run_time_more_args(dif,arg, arg2 = "", how_many_times = 1):
+def calculate_run_time_more_args(func,arg, arg2 = "", how_many_times = 1):
     sumTime = 0
     for i in range(0, how_many_times):
         start = time.perf_counter()
-        dif(arg, arg2)
+        func(arg, arg2)
         end = time.perf_counter()
         sumTime += (end - start)
     return sumTime
