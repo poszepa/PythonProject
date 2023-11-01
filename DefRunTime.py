a1d549ce30b5e1c401a0023e9d6a523b87bcccba
Kamil Poszepczyński <93219293+poszepa@users.noreply.github.com>
2023-11-01 20:28:33 +0100

    add method with one more arg

--- DefRunTime.py
+++ DefRunTime.py
@@ -8,3 +8,13 @@ def calculate_run_time(dif,arg, how_many_times = 1):
         end = time.perf_counter()
         sumTime += (end - start)
     return sumTime
+
+
+def calculate_run_time_more_args(dif,arg, arg2 = "", how_many_times = 1):
+    sumTime = 0
+    for i in range(0, how_many_times):
+        start = time.perf_counter()
+        dif(arg, arg2)
+        end = time.perf_counter()
+        sumTime += (end - start)
+    return sumTime
