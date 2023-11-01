d131d12971fa2b6a36585f07c6f101a5ee4b0a28
Kamil Poszepczyński <93219293+poszepa@users.noreply.github.com>
2023-11-01 18:55:13 +0100

    Add several methods to sum every number in range

---
+++ SumNumbersInRange.py
@@ -0,0 +1,24 @@
+"""
+Write a program, which calculate sum every number from 1 to input a number.
+"""
+
+
+def sumNumberInRange(endOfRange):
+    return sum(range(1, endOfRange + 1))
+
+
+def sumNumberInLoop(endOfRange):
+    sum = 0
+    for number in range(1, endOfRange + 1):
+        sum += number
+    return sum
+
+def sumNumberInGenerator(endOfRange):
+    numberGenerator = [
+        number
+        for number in range(1, endOfRange + 1)
+    ]
+    return sum(numberGenerator)
+
+def sumNumberByArythmetic(endOfRange):
+    return (1 + endOfRange) / 2 * endOfRange
