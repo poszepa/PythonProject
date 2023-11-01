b80de90f66851cc5a06495fc8df95ba58f11c90e
Kamil Poszepczyński <93219293+poszepa@users.noreply.github.com>
2023-11-01 18:54:35 +0100

    Add every method from SumNumbersInRange.py

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
 import Enum
+import SumNumbersInRange
 
 if __name__ == '__main__':
-    Enum.program_to_calculate_area()
+ #   Enum.program_to_calculate_area()
+    print(SumNumbersInRange.sumNumberInRange(10000))
+    print(SumNumbersInRange.sumNumberInLoop(10000))
+    print(SumNumbersInRange.sumNumberInGenerator(10000))
+    print(SumNumbersInRange.sumNumberByArythmetic(10000))
