a86ab2db13acd3ccef57b6177f468772a7fedd76
Kamil Poszepczyński <93219293+poszepa@users.noreply.github.com>
2023-11-03 19:54:23 +0100

    Read every line in file

--- OperationOnFiles.py
+++ OperationOnFiles.py
@@ -1,8 +1,12 @@
 
-def file():
+def fileSave():
     with open("test.txt", "w") as file:
         file.write("new line")
 
 
+def readLine():
+    with open("test.txt", "r", encoding="UTF-8") as file:
+        for line in file:
+            print(line)
 
         #With as working like open and close the file. We not need to rembember to close the file.
